93fe1ec16a6f214b18ef9458b807fb758235cfd6
Dylan Mann-Krzisnik <dylan.mann-krzisnik@mail.mcgill.ca>
2026-02-24 14:31:31 -0500

    Refactor mouse brain spatial RNA-ATAC script to load aligned source and target data, update variable names, and optimize feature selection for GPU memory. Adjust spatial graph and training processes to use new data structure.

--- scripts/mouse_brain_spatial_rna_atac.py
+++ scripts/mouse_brain_spatial_rna_atac.py
@@ -54,61 +54,56 @@ if shutil.which("bedtools") is None:
         "bedtools is required for Cal_gene_peak_Net_new. Install bedtools and ensure sortBed is available on PATH."
     )
 
-base_path = os.path.join(os.getenv("DATAPATH"), "Spatial_ATAC_RNA", "mouse", "spatial_omics")
+base_path = os.path.join(os.getenv("DATAPATH"), "aligned_data")
 
-#%% load RNA data
-rna_file_name = os.path.join(base_path, "P22_RNA.h5ad")
+#%% load source data
+source_rna = sc.read_h5ad(os.path.join(base_path, "source_rna_aligned.h5ad"))
+source_atac = sc.read_h5ad(os.path.join(base_path, "source_atac_aligned.h5ad"))
 
-adata1 = sc.read_h5ad(rna_file_name)
-adata1.obsm["spatial"] = adata1.obsm["spatial"][:, [1, 0]] * -1
-adata1
+source_rna.obsm["spatial"] = source_rna.obsm["spatial"][:, [1, 0]] * -1
+source_atac.obsm["spatial"] = source_atac.obsm["spatial"][:, [1, 0]] * -1
 
-#%% load ATAC data
-atac_file_name = os.path.join(base_path, "P22_ATAC_lsi.h5ad")
-
-adata2 = sc.read_h5ad(atac_file_name)
-adata2.obsm["spatial"] = adata2.obsm["spatial"][:, [1, 0]] * -1
-adata2
-
-#%% compute highly variable genes and peaks
-if 'P22_RNA.h5ad' not in rna_file_name:
-    sc.pp.filter_genes(adata1, min_cells=3)
-    sc.pp.normalize_total(adata1, target_sum=1e4)
-    sc.pp.log1p(adata1)
-    sc.pp.highly_variable_genes(adata1, n_top_genes=2000)
-
-if 'P22_ATAC_lsi.h5ad' not in atac_file_name:
-    sc.pp.filter_genes(adata2, min_cells=3)
-    sc.pp.normalize_total(adata2, target_sum=1e4)
-    sc.pp.log1p(adata2)
-    sc.pp.highly_variable_genes(adata2, n_top_genes=10000)
+#%% load target data
+target_rna = sc.read_h5ad(os.path.join(base_path, "target_rna_aligned.h5ad"))
+target_atac = sc.read_h5ad(os.path.join(base_path, "target_atac_aligned.h5ad"))
 
 #%% TMP - redo HVG to limit number of features to fit inside GPU memory
 if socket.gethostname() != 'ri-muhc-gpu':
-    adata1.var['highly_variable'] = False
-    adata2.var['highly_variable'] = False
 
+    source_rna.var['highly_variable'] = False
+    source_atac.var['highly_variable'] = False
+
+    target_rna.var['highly_variable'] = False
+    target_atac.var['highly_variable'] = False
+    
     top_N_genes = 2000
     top_N_peaks = 10000
-    adata1.var.loc[adata1.var['highly_variable_rank'].le(top_N_genes - 1), 'highly_variable'] = True
-    adata2.var.loc[adata2.var['highly_variable_rank'].le(top_N_peaks - 1), 'highly_variable'] = True
+    source_rna.var.loc[source_rna.var['highly_variable_rank'].le(top_N_genes - 1), 'highly_variable'] = True
+    source_atac.var.loc[source_atac.var['highly_variable_rank'].le(top_N_peaks - 1), 'highly_variable'] = True
+
+    target_rna.var.loc[
+        target_rna.var_names.isin(source_rna.var_names[source_rna.var['highly_variable']]),
+        'highly_variable'] = True
+    target_atac.var.loc[
+        target_atac.var_names.isin(source_atac.var_names[source_atac.var['highly_variable']]),
+        'highly_variable'] = True
 
 #%% spatial graph
-MultiGATE.Cal_Spatial_Net(adata1, rad_cutoff=40)
-MultiGATE.Stats_Spatial_Net(adata1)
+MultiGATE.Cal_Spatial_Net(source_rna, rad_cutoff=40)
+MultiGATE.Stats_Spatial_Net(source_rna)
 
-MultiGATE.Cal_Spatial_Net(adata2, rad_cutoff=40)
-MultiGATE.Stats_Spatial_Net(adata2)
+MultiGATE.Cal_Spatial_Net(source_atac, rad_cutoff=40)
+MultiGATE.Stats_Spatial_Net(source_atac)
 
-adata1 = adata1[:, adata1.var['highly_variable']]
-adata2 = adata2[:, adata2.var['highly_variable']]
+source_rna = source_rna[:, source_rna.var['highly_variable']]
+source_atac = source_atac[:, source_atac.var['highly_variable']]
 
 gtf_path = os.path.join(os.getenv("DATAPATH"), "gene_annotations", "gencode.vM25.chr_patch_hapl_scaff.annotation.gtf.gz")
 if not os.path.exists(gtf_path):
     raise FileNotFoundError("GTF annotation file not found: {}".format(gtf_path))
 
-MultiGATE.Cal_gene_peak_Net_new(adata1, adata2, 150000, file=gtf_path)
-adata1.uns['gene_peak_Net'] = adata2.uns['gene_peak_Net']
+MultiGATE.Cal_gene_peak_Net_new(source_rna, source_atac, 150000, file=gtf_path)
+source_rna.uns['gene_peak_Net'] = source_atac.uns['gene_peak_Net']
 
 #%% running MultiGATE
 num_epochs = int(os.getenv("MULTIGATE_EPOCHS", "3000"))
@@ -116,13 +111,13 @@ if num_epochs <= 0:
     raise ValueError("MULTIGATE_EPOCHS must be a positive integer.")
 
 print("Training epochs:", num_epochs)
-adata1, adata2 = MultiGATE.train_MultiGATE(adata1, adata2, bp_width=400, n_epochs=num_epochs)
+source_rna, source_atac = MultiGATE.train_MultiGATE(source_rna, source_atac, bp_width=400, n_epochs=num_epochs)
 
 #%% clustering with Muon's WNN clustering
-sc.pp.neighbors(adata1)
-sc.pp.neighbors(adata2)
+sc.pp.neighbors(source_rna)
+sc.pp.neighbors(source_atac)
 
-mdata = mu.MuData({"rna": adata1, "atac": adata2})
+mdata = mu.MuData({"rna": source_rna, "atac": source_atac})
 mu.pp.neighbors(mdata)
 
 mu.tl.umap(mdata)
@@ -130,15 +125,15 @@ sc.tl.leiden(mdata, resolution=1.5)
 
 # Replicate outputs of wnn_R: propagate cluster labels and UMAP coordinates
 # back to the individual AnnData objects so downstream code is unaffected.
-for ad in [adata1, adata2]:
+for ad in [source_rna, source_atac]:
     ad.obs['wnn'] = mdata.obs['leiden'].astype(int).astype('category')
     ad.obsm['X_umap'] = mdata.obsm['X_umap']
 
 #%% visualize results
 plt.rcParams["figure.figsize"] = (7, 3)
 fig, axs = plt.subplots(1, 2)
-sc.pl.embedding(adata1, basis="spatial", color="wnn", s=20, show=False, title='MultiGATE Spatial', ax=axs[0], legend_loc='None')
-sc.pl.umap(adata1, color="wnn", title='MultiGATE UMAP', ax=axs[1], size=20)
+sc.pl.embedding(source_rna, basis="spatial", color="wnn", s=20, show=False, title='MultiGATE Spatial', ax=axs[0], legend_loc='None')
+sc.pl.umap(source_rna, color="wnn", title='MultiGATE UMAP', ax=axs[1], size=20)
 plt.tight_layout()
 plt.show()
 
